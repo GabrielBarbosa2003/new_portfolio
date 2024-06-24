144d71f141cabd689febc2857f3ca40f6abb65ed
gabriel <gabrieldecarvalhu@gmail.com>
2024-06-24 18:54:44 -0300

    titulos pg work

--- src/pages/Works/index.jsx
+++ src/pages/Works/index.jsx
@@ -3,8 +3,27 @@ import './works.css'
 
 export default function Works() {
   return (
-    <div className='mid_test' >
-      <h1>Work</h1>
+    <div className='container-slides'>
+      <div className='slider' >
+        <div className='slide-titles'>
+          <div class="title">
+            <h1>Neo Forge Towers</h1>
+          </div>
+          <div class="title"><h1>Arcadian Complex</h1></div>
+          <div class="title"><h1>Shadowline Spire</h1></div>
+          <div class="title"><h1>Echo Nexus Habitat</h1></div>
+          <div class="title"><h1>Cascade Enclave</h1></div>
+          <div class="title"><h1>Prism Sector</h1></div>
+          <div class="title"><h1>Iron Eden Colony</h1></div>
+          <div class="title"><h1>Neo Forge Towers</h1></div>
+          <div class="title"><h1>Arcadian Complex</h1></div>
+        </div>
+        <div className='slide-images'>
+
+        </div>
+      </div>
+
     </div>
+
   )
 }
