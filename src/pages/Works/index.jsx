c9f27b8da7807a166800a54a28941f1ef7200c8b
gabriel <gabrieldecarvalhu@gmail.com>
2024-06-25 11:34:45 -0300

    slider pronto porém sem imgs

--- src/pages/Works/index.jsx
+++ src/pages/Works/index.jsx
@@ -1,10 +1,88 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './works.css'
+import gsap from 'gsap';
 
 export default function Works() {
+  var currentIndex = 1;
+  var totalSlides = 7;
   const atualizaSlideAtivo = () => {
-    
+    document.querySelectorAll(".title").forEach((el, index) => {
+      if (index === currentIndex) {
+        el.classList.add("active")
+      } else {
+        el.classList.remove("active")
+      }
+    })
+
   }
+
+  const handleSlider = () => {
+    if (currentIndex < totalSlides) {
+      currentIndex++;
+    } else {
+      currentIndex = 1;
+    }
+
+    gsap.to(".slide-titles", {
+      onStart: () => {
+        setTimeout(() => {
+          atualizaSlideAtivo(currentIndex + 1)
+        }, 100)
+      },
+      x: `-${(currentIndex - 1) * 11.1111}%`,
+      duration: 2,
+      ease: "power4.out",
+    })
+  }
+
+  const updateImages = (imageNumber) => {
+    const srcImg = `../../assets/img/works/${imageNumber}.jpg`;
+    const imgTop = document.createElement("img");
+    const imgBottom = document.createElement("img");
+
+    imgTop.src = imgSrc;
+    imgBottom.src = imgSrc;
+
+
+    imgTop.style.clipPath = "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)";
+    imgBottom.style.clipPath = "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)";
+    imgTop.style.transform = "scale(2)";
+    imgBottom.style.transform = "scale(2)";
+
+    document.querySelector(".img-top").appendChild(imgTop);
+    document.querySelector(".img-bottom").appendChild(imgBottom);
+
+    gsap.to([imgTop, imgBottom], {
+      clipPath: "polygon(100% 0%, 0% 0%, 0% 100%, 100% 100%)",
+      transform: "scale(1)",
+      duration: 2,
+      ease: "power4.out",
+      stagger: 0.15,
+      onComplete: trimExcessImages,
+    });
+
+  }
+
+  const trimExcessImages = () => {
+    const selectors = [".img-top", ".img-bottom"];
+
+    selectors.forEach((selectors) => {
+      const container = document.querySelector(selector);
+      const images = Array.from(container.querySelectorAll("img"));
+      const excessCount = images.length - 5;
+
+      if (excessCount > 0) {
+        images
+          .slice(0, excessCount)
+          .forEach((image) => container.removeChild(image));
+      }
+    })
+  }
+
+  useEffect(() => {
+    document.addEventListener("click", handleSlider);
+
+  })
   return (
     <div className='container-slides'>
       <div className='slider' >
