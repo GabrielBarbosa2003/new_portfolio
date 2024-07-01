766f9656bfc07c4b9509f3caad4efa98971a5edf
gabriel <gabrieldecarvalhu@gmail.com>
2024-07-01 15:42:28 -0300

    arrumando slides

--- src/pages/Works/index.jsx
+++ src/pages/Works/index.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './works.css'
+
+import img1 from '../../assets/img/works/img1.jpg'
+import img2 from '../../assets/img/works/img2.jpg'
+import img3 from '../../assets/img/works/img3.jpg'
 import gsap from 'gsap';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -9,47 +13,6 @@ import 'swiper/css/grid';
 import { Navigation, Grid } from 'swiper/modules'
 
 export default function Works() {
-
-  var currentIndex = 1;
-  var totalSlides = 7;
-
-  const atualizaSlideAtivo = () => {
-    document.querySelectorAll(".title").forEach((el, index) => {
-      if (index === currentIndex) {
-        el.classList.add("active")
-      } else {
-        el.classList.remove("active")
-      }
-    })
-
-  }
-  const handleSlider = () => {
-    if (currentIndex < totalSlides) {
-      currentIndex++;
-    } else {
-      currentIndex = 1;
-    }
-
-    gsap.to(".slide-titles", {
-      onStart: () => {
-        setTimeout(() => {
-          atualizaSlideAtivo();
-        }, 100);
-
-        updateImages(currentIndex + 1);
-      },
-      x: `-${(currentIndex - 1) * 11.1111}%`,
-      duration: 2,
-      ease: "power4.out",
-    });
-  };
-
-  // useEffect(() => {
-  //   document.addEventListener("click", handleSlider);
-
-  //   updateImages(2);
-  //   atualizaSlideAtivo();
-  // })
 
   const titulos = [
     { title: "Arcadian Complex", id: 1 },
@@ -63,7 +26,7 @@ export default function Works() {
     { title: "Echo Nexus Habitat", id: 9 }
   ]
 
-  const [currentSlideId, setCurrentSlideId] = useState(null);
+  const [currentSlideId, setCurrentSlideId] = useState(0);
 
   const handleSlideChange = (swiper) => {
     const currentIndex = swiper.activeIndex;
@@ -71,6 +34,61 @@ export default function Works() {
     const currentId = currentSlide.getAttribute('data-id');
     setCurrentSlideId(currentId);
   };
+
+  const imageWorks = {
+    0: img1,
+    1: img2,
+    2: img3
+  }
+
+  const updateImages = (imageNumber) => {
+    const imgSrc = imageWorks[imageNumber]
+    const imgTop = document.createElement("img");
+    const imgBottom = document.createElement("img");
+
+
+    imgTop.src = imageWorks[imageNumber];
+    imgBottom.src = imageWorks[imageNumber];
+    console.log(imageNumber)
+
+    
+
+
+    imgTop.style.clipPath = "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)";
+    imgBottom.style.clipPath = "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)";
+    imgTop.style.transform = "scale(2)";
+    imgBottom.style.transform = "scale(2)";
+
+    document.querySelector(".img-top").appendChild(imgTop);
+    document.querySelector(".img-bottom").appendChild(imgBottom);
+
+    gsap.to([imgTop, imgBottom], {
+      clipPath: "polygon(100% 0%, 0% 0%, 0% 100%, 100% 100%)",
+      transform: "scale(1)",
+      duration: 2,
+      ease: "power4.out",
+      stagger: 0.15,
+      //onComplete: trimExcessImages,
+    });
+  };
+
+
+  function trimExcessImages() {
+    const selectors = [".img-top", ".img-bottom"];
+
+    selectors.forEach((selector) => {
+      const container = document.querySelector(selector);
+      const images = Array.from(container.querySelectorAll("img"));
+      const excessCount = images.length - 5;
+
+      if (excessCount > 0) {
+        images
+          .slice(0, excessCount)
+          .forEach((image) => container.removeChild(image));
+      }
+    });
+  }
+
 
 
   return (
@@ -84,28 +102,37 @@ export default function Works() {
             slidesPerView={3}
             loop={true}
             spaceBetween={10}
-            onSlideChange={handleSlideChange}
-            >
+            onSlideChange={(swiper) => {
+              handleSlideChange(swiper);
+              updateImages(currentSlideId)
+            }}
+          >
 
-            {titulos.map((titulo,index) => (
+            {titulos.map((titulo, index) => (
               <SwiperSlide key={titulo.id} data-id={titulo.id}>
                 <div className={index == currentSlideId ? "title_active" : "title"} >
                   <h1>{titulo.id}</h1>
                 </div>
               </SwiperSlide>
-              
-              
-              
-            )) }
+
+
+
+            ))}
 
           </Swiper>
 
         </div>
-        <div className='slide-images'>
-          <div className='img-top'></div>
-          <div className='img-bottom'></div>
 
+      </div>
+      <div className='slide-images'>
+        <div className='img-top'>
+          {/* <img src={imageWorks[currentSlideId]} alt='' /> */}
         </div>
+
+        <div className='img-bottom'>
+          {/* <img src={imageWorks[currentSlideId]} alt='' /> */}
+        </div>
+
       </div>
 
     </div>
